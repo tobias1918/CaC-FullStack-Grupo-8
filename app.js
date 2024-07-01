25590213b9d90749a2ab0b728e843839bd927e79
tobias1918 <tobilp86@gmail.com>
2024-07-01 16:25:03 -0300

    actualizacion de shop para que consuma datos de la db

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const path = require('path');
+
 const app= express();
 
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'src', 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/',require('./src/routes/mainRoutes'))
 
--- src/routes/mainRoutes.js
+++ src/routes/mainRoutes.js
@@ -3,10 +3,10 @@ const router = express.Router();
 
 const { conn } = require('../db/dbconnection'); // Importa la conexión desde tu módulo
 
-router.get('/productos', async (req, res) => {
+router.get('/shop', async (req, res) => {
     try {
         const sqlQuery = `
-            SELECT p.id, p.nombre, p.descripcion,
+            SELECT p.id, p.nombre, p.descripcion,p.precio,p.imagen,
                    m.id as marca_id, m.nombre as nombre_marca, m.descripcion as desc_marca,
                    c.id as categoria_id, c.nombre as nombre_categoria
             FROM Producto p
@@ -20,6 +20,45 @@ router.get('/productos', async (req, res) => {
             id: row.id,
             nombre: row.nombre,
             descripcion: row.descripcion,
+            precio:row.precio,
+            imagen:row.imagen,
+            marca_id: {
+                id: row.marca_id,
+                nombre: row.nombre_marca,
+                descripcion: row.desc_marca
+            },
+            categoria_id: {
+                id: row.categoria_id,
+                nombre: row.nombre_categoria
+            }
+        }));
+
+        res.render('shop',{productos:productos});
+    } catch (err) {
+        console.error('Error al obtener productos:', err);
+        res.status(500).send('Error al obtener productos');
+    }
+});
+
+router.get('/productos', async (req, res) => {
+    try {
+        const sqlQuery = `
+            SELECT p.id, p.nombre, p.descripcion,p.precio,p.imagen,
+                   m.id as marca_id, m.nombre as nombre_marca, m.descripcion as desc_marca,
+                   c.id as categoria_id, c.nombre as nombre_categoria
+            FROM Producto p
+            INNER JOIN Marca m ON p.marca_id = m.id
+            INNER JOIN Categoria c ON p.categoria_id = c.id
+        `;
+        const [rows] = await conn.query(sqlQuery);
+
+        // Formatear json
+        const productos = rows.map(row => ({
+            id: row.id,
+            nombre: row.nombre,
+            descripcion: row.descripcion,
+            precio:row.precio,
+            imagen:row.imagen,
             marca_id: {
                 id: row.marca_id,
                 nombre: row.nombre_marca,
